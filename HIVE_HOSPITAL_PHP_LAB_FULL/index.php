1d9ce8bad6212803f48cc266221547307bbd43d0
GhostNinja <121746490+DghostNinja@users.noreply.github.com>
2025-03-31 20:21:53 +0100

    Update index.php

--- HIVE_HOSPITAL_PHP_LAB_FULL/index.php
+++ HIVE_HOSPITAL_PHP_LAB_FULL/index.php
@@ -1,1 +1,23 @@
-<html><head><title>HIVE CONSULT VULNERABLE HOSPITAL</title><link rel='stylesheet' href='assets/css/style.css'></head><body><h1>Welcome to HIVE CONSULT</h1></body></html>
+<?php echo '
+<html>
+<head>
+    <title>HIVE CONSULT VULNERABLE HOSPITAL</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="background-image">
+            <img src="images/image1.jpg" alt="Background">
+        </div>
+        <div class="form-box">
+            <h1>Welcome to HIVE CONSULT</h1>
+            <form>
+                <input type="text" placeholder="Username">
+                <input type="password" placeholder="Password">
+                <button>Login</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>';
+?>
